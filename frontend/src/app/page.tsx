b1648facd4743ce712cb2a758758a8306c990375
drew goldstein <drew@degstn.com>
2023-12-04 14:40:01 -0500

    updated image color schemes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-25 ">
       <div className="flex flex-col items-center min-h-[calc(100vh-44px)] justify-center">
-        <Image src="/steliologov1s.svg" height={100} width={250} alt={''} />
+      <picture>
+        <source srcSet="/steliologov1sd.svg" media="(prefers-color-scheme: dark)" />
+        <Image
+            src="/steliologov1s.svg"
+            alt="My image"
+            width={250}
+            height={100}
+        />
+      </picture>
         <h1 className="text-3xl tracking-tight max-w-3xl font-regular mb-4 mt-10">
         Coming soon.
         </h1>
