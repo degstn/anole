2c924bbf8141c5a4e4c05f265c8ab9007dbbbd9e
drew goldstein <drew@degstn.com>
2023-12-05 00:55:17 -0500

    added social media links to global footer

--- frontend/src/app/components/Footer.js
+++ frontend/src/app/components/Footer.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-900 bg-opacity-0">
-      <div className="grid grid-cols-1 gap-8 py-12 text-black transition-colors duration-150 border-b lg:grid-cols-12 border-zinc-600 bg-zinc-900 bg-opacity-0">
+      <div className="grid grid-cols-1 gap-8 py-12 pb-20 text-black transition-colors duration-150 border-b lg:grid-cols-12 border-zinc-600 bg-zinc-900 bg-opacity-0">
         <div className="col-span-1 lg:col-span-2">
           <Link
             href="/"
@@ -96,6 +96,7 @@ export default function Footer() {
                 Case Studies
               </Link>
             </li>
+            {/* need an arrow icon for external site link */}
             <li className="py-3 md:py-0 md:pb-2">
               <Link
                 href="/"
@@ -145,12 +146,13 @@ export default function Footer() {
                 Plan of Work Log
               </Link>
             </li>
+            {/* need an arrow icon for external site link */}
             <li className="py-3 md:py-0 md:pb-2">
               <Link
-                href="/"
+                href="https://tsaweb.org"
                 className="text-black transition duration-150 ease-in-out hover:text-gray-800"
               >
-                NTSA
+                NTSA 
               </Link>
             </li>
           </ul>
@@ -205,7 +207,7 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-start py-2 pb-20 space-y-4 md:flex-row bg-zinc-900 bg-opacity-0">
+      <div className="flex flex-col items-center justify-start py-0 pb-20 space-y-4 md:flex-row bg-zinc-900 bg-opacity-0">
         <div>
           <span>
             Copyright &copy; {new Date().getFullYear()} Stelio
@@ -225,7 +227,7 @@ export default function Footer() {
         </div>
       <div className="col-span-1 lg:col-span-2">
           <ul className="flex flex-col flex-initial md:flex-1">
-            <li className="py-3 md:py-0 md:pb-4 px-0">
+            <li className="py-3 md:py-0 md:pb-4 px-0 pr-5">
               <Link
                 href="/"
                 className="text-black transition duration-150 ease-in-out hover:text-gray-800"
@@ -235,7 +237,61 @@ export default function Footer() {
             </li>
           </ul>
         </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ul className="flex flex-col flex-initial md:flex-1">
+            <li className="py-3 md:py-0 md:pb-4 px-5">
+              <Link
+                href="https://x.com/fltsa"
+                className="text-black transition duration-150 ease-in-out hover:text-gray-800"
+              >
+                X  
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ul className="flex flex-col flex-initial md:flex-1">
+            <li className="py-3 md:py-0 md:pb-4 px-5">
+              <Link
+                href="https://instagram.com/fltsa"
+                className="text-black transition duration-150 ease-in-out hover:text-gray-800"
+              >
+                 Instagram 
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ul className="flex flex-col flex-initial md:flex-1">
+            <li className="py-3 md:py-0 md:pb-4 px-5">
+              <Link
+                href="https://github.com/degstn/anole
+                "
+                className="text-black transition duration-150 ease-in-out hover:text-gray-800"
+              >
+                GitHub  
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
 }
+
+function ArrowIcon() {
+    return (
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+          fill="currentColor"
+        />
+      </svg>
+    );
+  }
