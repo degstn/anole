baa53d38924e06f6e17962ec50beb45c25abc1c4
drew goldstein <drew@degstn.com>
2023-12-22 23:36:39 -0500

    fixed scrolling behavior in MobileNav; link behavior (+ styling e.g. underline/hover [mobile has no hover behavior]) for MobileNavItem

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -5,7 +5,8 @@ import { usePathname } from 'next/navigation';
 import { Suspense } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react'; 
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
 
 const navItems = {
   '/product': {
@@ -26,9 +27,23 @@ const  Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
   const MobileNav = () => {
     setIsOpen(!isOpen);
   }
+
+  function closeMobileNav() {
+    setIsOpen(false);
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isOpen]);
 
   return (
     <nav className="fixed w-full py-5 px-10">
@@ -61,7 +76,7 @@ const  Navbar = () => {
         </div>
         <div className={
           isOpen 
-          ? "fixed left-0 top-0 bg-white dark:bg-neutral-900 h-screen w-screen overscroll-none" 
+          ? "fixed left-0 top-0  bg-neutral-900 h-screen w-screen overscroll-none" 
           : "hidden"
           }
           >
@@ -72,9 +87,9 @@ const  Navbar = () => {
             </div>
             <div className="flex-col text-4xl font-semibold text-white py-10 px-3">
             <Suspense fallback={null}>
-                {Object.entries(navItems).map(([path, { name }]) => {
-                  return <NavItem key={path} path={path} name={name} />;
-                })}
+            {Object.entries(navItems).map(([path, { name }]) => {
+  return <MobileNavItem key={path} path={path} name={name} closeMobileNav={closeMobileNav} />;
+})}
             </Suspense>
             </div>
         </div>
@@ -116,6 +131,41 @@ function NavItem({ path, name }: { path: string; name: string }) {
   );
 }
 
+function MobileNavItem({ path, name, closeMobileNav }: { path: string; name: string; closeMobileNav: () => void }) {
+  let pathname = usePathname() || '/';
+  if (pathname.includes('/research/')) {
+    pathname = '/research';
+  }
+  let isActive = path === pathname;
+
+  return (
+    <div onClick={() => { closeMobileNav();}}>
+    <Link
+      key={path}
+      href={path}
+      className=
+        'transition-all underline-offset-4 flex align-middle'
+      
+    >
+      <span className="relative py-3 px-2">
+        {name}
+        {path === pathname ? (
+          <motion.div
+            className="absolute h-[1px] top-8 mx-2 inset-0 bg-black dark:bg-neutral-800 z-[-1] dark:bg-gradient-to-r from-transparent to-neutral-900"
+            layoutId="sidebar"
+            transition={{
+              type: 'spring',
+              stiffness: 350,
+              damping: 30,
+            }}
+          />
+        ) : null}
+      </span>
+    </Link>
+    </div>
+  );
+}
+
 function OpenMenu() {
   return (
     <svg 
@@ -141,12 +191,12 @@ function CloseMenu() {
       width="40" 
       height="40" 
       viewBox="0 0 40 40" 
-      fill="none" 
+      fill="white" 
       xmlns="http://www.w3.org/2000/svg"
     >
       <path 
         d="m10.543 30.958-1.5-1.5 9.5-9.458-9.5-9.458 1.5-1.5 9.458 9.5 9.458-9.5 1.5 1.5-9.5 9.458 9.5 9.458-1.5 1.5-9.458-9.5-9.458 9.5Z" 
-        fill="currentFill"
+        fill="000000"
         />
     </svg>
   );
