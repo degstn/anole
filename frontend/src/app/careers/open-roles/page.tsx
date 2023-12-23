e379670c7b8d692c726af8942fe7905da770f18a
drew goldstein <drew@degstn.com>
2023-12-23 10:21:46 -0500

    updated sorting for open roles page

--- frontend/src/app/careers/open-roles/page.tsx
+++ frontend/src/app/careers/open-roles/page.tsx
@@ -1,50 +1,86 @@
 'use client';
 
 import Link from 'next/link'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const originalJobs = [
+    { job: "Software Engineer", location: "Remote - Miami HQ", team:  "Engineering"},
+    { job: "Researcher", location: "US Remote", team:  "Research"},
+    { job: "Team Leader", location: "Miami HQ", team:  "Leadership"},
+    { job: "Software Engineer", location: "Remote - Miami HQ", team:  "Engineering"},
+    { job: "Senior Software Engineer", location: "Remote - Miami HQ", team:  "User Experience"},
+    { job: "Software Engineer", location: "Remote - Miami HQ", team:  "Web Infrastructure"},
+    { job: "Senior Software Engineer", location: "Remote - Miami HQ", team:  "Web Infrastructure"},
+    { job: "Campus Manager", location: "Miami HQ", team:  "HQ"},
+    { job: "Payroll Supervisor", location: "Remote - Miami HQ", team:  "HQ"},
+    { job: "Senior Campus Manager", location: "Remote - Miami HQ", team:  "Engineering"},
+    { job: "Senior Software Engineer", location: "Remote - Miami HQ", team:  "Researcher"},
+];
 
 export default function Page() {
-    const [location, setLocation] = useState('');
-    const [jobType, setJobType] = useState('');
+    const [location, setLocation] = useState("");
+    const [selectedLocation, setSelectedLocation] = useState("");
+    const [jobType, setJobType] = useState("");
+    const [jobs, setJobs] = useState(originalJobs);
+
+    useEffect(() => {
+        const filteredJobs = jobType === "" 
+            ? originalJobs 
+            : originalJobs.filter(job => job.team === jobType);
+        setJobs(filteredJobs);
+    }, [jobType]);
+
+    useEffect(() => {
+        let filteredJobs = originalJobs;
+    
+        if (jobType !== "") {
+            filteredJobs = filteredJobs.filter(job => job.team === jobType);
+        }
+    
+        if (selectedLocation !== "") {
+            filteredJobs = filteredJobs.filter(job => job.location === selectedLocation);
+        }
+    
+        setJobs(filteredJobs);
+    }, [jobType, selectedLocation]);
+
     return (
         <main className="flex min-h-screen flex-col items-stretch justify-between">
             <div className="mx-6">
                 <h1 className="text-5xl font-regular mb-16 mt-52">Open Roles</h1>
                 <hr className="border-t border-black w-full" />
                 <h2 className="text-3xl font-semibold mt-10 mb-5">Current Open Roles</h2>
-                <div className="flex justify-between mb-5">
-                    <select
-                        className="border border-gray-300 rounded-md px-4 py-2"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                    >
+                <div className="flex justify-start mb-5">
+                    <select value={selectedLocation} onChange={e => setSelectedLocation(e.target.value)}>
                         <option value="">All Locations</option>
-                        <option value="Remote">Remote</option>
+                        <option value="Remote - Miami HQ">Remote - Miami HQ</option>
+                        <option value="US Remote">US Remote</option>
                         <option value="Miami HQ">Miami HQ</option>
                     </select>
+
                     <select
                         className="border border-gray-300 rounded-md px-4 py-2"
                         value={jobType}
                         onChange={(e) => setJobType(e.target.value)}
                     >
-                        <option value="">All Job Types</option>
-                        <option value="Software Engineer">Software Engineer</option>
-                        <option value="Researcher">Researcher</option>
-                        <option value="Product Manager">Product Manager</option>
+                        <option value="">All teams</option>
+                        <option value="Engineering">Software Engineer</option>
+                        <option value="Research">Researcher</option>
+                        <option value="User Experience">User Experience</option>
+                        <option value="Leadership">Leadership Leader</option>
+                        <option value="Web Infrastructure">Web Infrastructure</option>
+                        <option value="HQ">HQ</option>
+                        
+                        
                     </select>
                 </div>
-                <h2 className="font-semibold mb-16">Showing 21 Jobs.</h2>
-            
+                <h2 className="font-semibold mb-16">Showing 11 Jobs.</h2>
             <div className="grid gap-0">
-                {[
-                    { job: "Software Engineer", location: "Remote - Miami HQ" },
-                    { job: "Researcher", location: "US Remote" },
-                    { job: "Product Manager", location: "Miami HQ" }
-                ].map((jobDetail, index) => (
+                {jobs.map((job, index) => (
                     <div key={index} className="border-t border-black py-2 flex justify-between items-center">
                         <div>
-                            <h3 className="font-bold">{jobDetail.job}</h3>
-                            <p>{jobDetail.location}</p>
+                            <h3 className="font-bold">{job.job}</h3>
+                            <p>{job.location} - {job.team}</p>
                         </div>
                         <Link href={`https://www.greenhouse.com`}>
                             <span className="flex items-center underline">
