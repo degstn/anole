a764d789529c2f9b3cf584f4c904ce7d0405a58a
drew goldstein <drew@degstn.com>
2024-02-02 21:15:38 -0500

    updated product overview

--- frontend/src/app/(client)/product/page.tsx
+++ frontend/src/app/(client)/product/page.tsx
@@ -59,41 +59,105 @@ export default function Page() {
                 </div>
             </div>
 
-            <div ref={overviewRef} id="overview" className="bg-lime-50 lg:py-28">
-                <div className="mx-6">
-                    <div className="text-5xl font-regular mb-6 mt-96">
-                        <picture>
-                            <Image
-                                src="/sp.svg"
-                                alt="Stelio Logo"
-                                width={250}
-                                height={100}
-                            />
-                        </picture>
+            <div ref={overviewRef} id="overview">
+        <h1 className="text-3xl lg:text-5xl tracking-tight max-w-3xl text-green-800 font-medium mb-2 mt-20 mx-6">
+        <span>Facilitate Personal Energy </span>
+        </h1>
+        <p className="text-2xl lg:text-3xl tracking-tight max-w-3xl text-neutral-800 font-medium mb-5 mx-6">
+        <span>Find popular energy incentives near you simply with your address. </span>
+        </p>
+        <div className='flex'>
+        <picture>
+          <Image
+            src="/s1.png"
+            alt=""
+            width={700}
+            height={400}
+          />
+        </picture>
+        
+        <p className="text-xl lg:text-3xl tracking-tight max-w-sm text-neutral-800 font-medium mt-32 mx-6">
+        <span>After a quick location check, a dynamic list of local and popular <span className="font-semibold text-green-900">incentivized base configurations</span> will be provided to start your journey.</span>
+        </p>
+        </div>
+        
+
+        <div>
+        <h1 className="text-3xl lg:text-5xl tracking-tight text-teal-800 font-medium mb-2 mt-20 mx-6 text-end">
+        <span>Maximize Value of Energy</span>
+        </h1>
+        <p className="text-2xl lg:text-3xl tracking-tight lg:ml-60 text-neutral-800 font-medium mb-5 mx-6 text-end">
+        <span>Stelio will give you an estimate on your energy bill, setup cost, and yearly energy bill. </span>
+        </p>
+        <div className='flex justify-end'>
+        <p className="text-xl lg:text-3xl tracking-tight max-w-sm text-neutral-800 font-medium mt-32 mx-6 text-end">
+        <span>Help <span className="font-semibold text-teal-900">visualize future costs</span> and changes in your bills, keeping you in the know about your expenses and what you can save.</span>
+        </p>
+        <picture>
+          <Image
+            src="/g1f.png"
+            alt="gs1"
+            width={800}
+            height={500}
+          />
+        </picture>
+        
+        
+        </div>
+          
+        <div>
+        <h1 className="text-3xl lg:text-5xl tracking-tight max-w-3xl text-fuchsia-800 font-medium mb-2 mt-20 mx-6">
+        <span>Prepare for the future</span>
+        </h1>
+        <p className="text-2xl lg:text-3xl tracking-tight max-w-3xl font-medium mb-5 mx-6">
+        <span>Find incentives for your home today to retrofit, or prepare for the future.</span>
+        </p>
+        <div className='flex'>
+        <picture>
+          <Image
+            src="/g0f.png"
+            alt="gs0"
+            width={800}
+            height={500}
+          />
+        </picture>
+        <p className="text-xl lg:text-3xl tracking-tight max-w-sm text-neutral-800 font-medium mt-32 mx-6">
+        <span>Discover the saving and benefits of retrofitting and upgrading to <span className="font-semibold text-fuchsia-900">increase the efficiency</span> of electricity in your household.</span>
+        </p>
+        </div>
+        </div>
+        </div>
+
+        <div className="lg:py-10">
+                <div className="mx-6 mt-20 mb-40 flex flex-col text-center items-center justify-center">
+                    <div className="text-6xl lg:text-8xl font-semibold mt-2 mb-4">
+                        Join the future <br/> of energy
                     </div>
-                    <p className="text-2xl font-semibold mt-2">
-                        Coming soon.
-                    </p>
-                </div>
-            </div>   
-            <div className="bg-white lg:py-10">
-                <div className="mx-6 mt-14 flex flex-col items-center justify-center">
-                    <div className="text-5xl font-semibold mt-2 mb-4">
-                        Get started with Stelio.
-                    </div>
-                    {/* <div className="text-2xl font-semibold mt-2">
-                        Coming soon.
-                    </div> */}
-                    <div className="w-40"> {/* Set the width of the container */}
+                    
+                    <div className="flex flex-col"> {/* Set the width of the container */}
                         <a
-                            href="/getstarted"
-                            className="inline-block border border-black text-black px-4 py-2 mt-10 mb-14 rounded-lg"
+                            href="/careers/open-roles"
+                            className="inline-block border text-white border-green-800 bg-green-700 px-4 py-2 mt-4 rounded-md"
                         >
                             Get started
+                        </a>
+                        <a
+                            href="/careers/open-roles"
+                            className="hover:underline inline-block text-black px-4 py-1 mt-4"
+                        >
+                            Learn more
                         </a>
                     </div>
                 </div>
             </div>
+        
+        {/* <a
+          href="/"
+          className="inline-block bg-black border border-black text-white px-10 py-2 mt-10 mb-20 rounded-lg mx-6"
+        >
+          Button
+        </a> */}
+      </div> 
         </main>
     )
 }
